109eedf48e2235a25135b4872979607da96c5695
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-27 12:04:04 -0800

    Create User.java
    user is different because it doesn't auto generate an id, it will be taken from the customer id

---
+++ src/main/java/com/banking/Spiral/entities/User.java
@@ -0,0 +1,47 @@
+package com.banking.Spiral.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+
+    // User ID is the same as customer id
+    // After creating a customer, the id will be used to make a user
+    @Id
+    private long user_id;
+    private String password;
+    private Role role;
+
+    public User() {};
+
+    public User(long user_id, String password, Role role) {
+        this.user_id = user_id;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
